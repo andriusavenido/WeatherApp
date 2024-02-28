c0ab234c5bf323410633c7ea8d7c205cf153db0b
Arlene Silva <andriusavenido@gmail.com>
2024-02-28 18:38:06 -0500

    Added basic css and html

---
+++ main.js
@@ -0,0 +1,6 @@
+// document.getElementById("searchForm").addEventListener("submit",function(event){
+//     event.preventDefault();
+    
+//     console.log("Hi")
+//     document.getElementById("weather-info").style.display = "block";
+// });
